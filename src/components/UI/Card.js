9773bad08e7d3eac6adfb0a72152da314bae1411
wantop <wantop@jbnu.ac.kr>
2023-03-27 01:39:42 +0900

    feat : card ui

---
+++ src/components/UI/Card.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const StyledCard = styled.div`
+  width : 20rem;
+  height: 20rem;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  border: 1px solid #d9d9d9;
+  overflow: hidden;
+  cursor : pointer;
+  transition: transform 0.3s;
+  box-shadow : 5px 10px 15px -3px rgba(0, 0, 0, 0.1);
+
+    &:hover {
+        transform: translateY(-0.5rem);
+    }
+`;
+
+const Card = ({ children }) => {
+  return <StyledCard>{children}</StyledCard>;
+};
+
+export default Card;
