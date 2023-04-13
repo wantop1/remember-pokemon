1c3511542201970107424e06f9b6d12c28f15228
wantop <wantop@jbnu.ac.kr>
2023-04-14 04:37:32 +0900

    feat : add Badge UI

--- src/components/Poke/PokeItem.js
+++ src/components/Poke/PokeItem.js
@@ -1,13 +1,26 @@
 import Card from "../UI/Card";
+import Badge from "../UI/Badge";
 import { POKEMON_IMAGE_URL } from "../../constants/image";
+import { TYPE_COLORS } from "../../constants/color";
 
 const PokeItem = ({ pokemon }) => {
+  const pokemonTypes = pokemon.types.map((type) => (
+    <Badge backgroundColor={TYPE_COLORS[type.type.name.toUpperCase()]}>
+      {type.type.name.toUpperCase()}
+    </Badge>
+  ));
+
   return (
     <li>
       <Card>
         <div>{pokemon.id}</div>
         <div>{pokemon.names[2].name}</div>
-        <img style={{width:"18rem"}} alt="poke-img" src={POKEMON_IMAGE_URL + pokemon.id + ".png"} />
+        <div>{pokemonTypes}</div>
+        <img
+          style={{ width: "18rem" }}
+          alt="poke-img"
+          src={POKEMON_IMAGE_URL + pokemon.id + ".png"}
+        />
       </Card>
     </li>
   );
---
+++ src/components/UI/Badge.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const StyledBadge = styled.span`
+  font-size: 0.75rem;
+  line-height: 1rem;
+  color: #ffffff;
+  padding: 0 0.75rem;
+  margin-right: 0.25rem;
+  border-radius: 0.25rem;
+  background-color: ${(props) => props.backgroundColor};
+`;
+
+const Badge = ({ children, backgroundColor }) => {
+  return (
+    <StyledBadge backgroundColor={backgroundColor}> {children} </StyledBadge>
+  );
+};
+
+export default Badge;
