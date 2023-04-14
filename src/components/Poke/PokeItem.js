cc0f9603cae194f4de04e5cf3e2fda21f5bbc7da
wantop <wantop@jbnu.ac.kr>
2023-04-14 14:33:39 +0900

    feat : add id,name  styling

--- src/components/Poke/PokeItem.js
+++ src/components/Poke/PokeItem.js
@@ -2,10 +2,28 @@ import Card from "../UI/Card";
 import Badge from "../UI/Badge";
 import { POKEMON_IMAGE_URL } from "../../constants/image";
 import { TYPE_COLORS } from "../../constants/color";
+import styled from "styled-components";
 
 const PokeItem = ({ pokemon }) => {
+  const IdTypography = styled.div`
+    color: #707070;
+  `;
+
+  const NameTypography = styled.div`
+    font-size: 1.5rem;
+  `;
+
+  const PokemonImg = styled.img`
+    width: 15rem;
+  `;
+
+  const id = "#" + pokemon.id.toString().padStart(3, "0");
+  const name = pokemon.names[2].name;
   const pokemonTypes = pokemon.types.map((type) => (
-    <Badge backgroundColor={TYPE_COLORS[type.type.name.toUpperCase()]}>
+    <Badge
+      key={type.slot}
+      backgroundColor={TYPE_COLORS[type.type.name.toUpperCase()]}
+    >
       {type.type.name.toUpperCase()}
     </Badge>
   ));
@@ -13,12 +31,11 @@ const PokeItem = ({ pokemon }) => {
   return (
     <li>
       <Card>
-        <div>{pokemon.id}</div>
-        <div>{pokemon.names[2].name}</div>
+        <IdTypography>{id}</IdTypography>
+        <NameTypography>{name}</NameTypography>
         <div>{pokemonTypes}</div>
-        <img
-          style={{ width: "18rem" }}
-          alt="poke-img"
+        <PokemonImg
+          alt="pokemon-img"
           src={POKEMON_IMAGE_URL + pokemon.id + ".png"}
         />
       </Card>
